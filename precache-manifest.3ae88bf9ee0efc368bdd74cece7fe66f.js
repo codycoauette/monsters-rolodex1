9a8354b8a4216a102d73852c14711d3e56ab1c37
codycoauette <codycoauette@yahoo.com>
2020-04-03 13:33:43 -0500

    Updates

---
+++ precache-manifest.3ae88bf9ee0efc368bdd74cece7fe66f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb7caaa96319c36cd5acabd12397386f",
+    "url": "/monsters-rolodex1/index.html"
+  },
+  {
+    "revision": "597b9ccb17572b02568c",
+    "url": "/monsters-rolodex1/static/css/main.5ba66bea.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex1/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex1/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "597b9ccb17572b02568c",
+    "url": "/monsters-rolodex1/static/js/main.f5a3edaf.chunk.js"
+  },
+  {
+    "revision": "ab085383ef6945e1bcfe",
+    "url": "/monsters-rolodex1/static/js/runtime-main.3aaa116b.js"
+  }
+]);
